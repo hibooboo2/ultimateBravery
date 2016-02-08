73f30761fbf4cb02296ceb234272d54cb124833c
James Jeffrey <wizardofmath@gmail.com>
2016-02-08 14:51:31 -0700

    add xargs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ cd $(dirname $0)/..
 git fetch --all
 git checkout origin/master
 
-[[ -z "$(docker ps -a | grep ultimateBravery)" ]] && docker rm -fv ultimateBravery
+[[ -z "$(docker ps -a | grep ultimateBravery | xargs)" ]] && docker rm -fv ultimateBravery
 docker build -t ultimate-bravery .
 docker run --restart=always -d -p 9001:8000 -e RIOT_API_KEY=$RIOT_API_KEY --name=ultimateBravery ultimate-bravery
