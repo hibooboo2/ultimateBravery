23ae2daad3165bbe60d46c7aec114091cfe1bfba
James Jeffrey <wizardofmath@gmail.com>
2016-02-08 14:50:41 -0700

    Update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ cd $(dirname $0)/..
 git fetch --all
 git checkout origin/master
 
-docker rm -fv ultimateBravery;
-docker build -t ultimateBravery .
-docker run --restart=always -d -p 9001:8000 -e RIOT_API_KEY=$RIOT_API_KEY --name=ultimateBravery ultimateBravery
+[[ -z "$(docker ps -a | grep ultimateBravery)" ]] && docker rm -fv ultimateBravery
+docker build -t ultimate-bravery .
+docker run --restart=always -d -p 9001:8000 -e RIOT_API_KEY=$RIOT_API_KEY --name=ultimateBravery ultimate-bravery
