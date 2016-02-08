78c16894cecda07ba128682ccb8c6f3510c6f46d
James Jeffrey <wizardofmath@gmail.com>
2016-02-08 14:56:16 -0700

    Final Deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,6 @@ set -e
 
 cd $(dirname $0)/..
 
-git fetch --all
-git checkout origin/master
-
-[[ -z "$(docker ps -a | grep ultimateBravery | xargs)" ]] && docker rm -fv ultimateBravery
+docker rm -fv ultimateBravery || echo "No container for ultimateBravery".
 docker build -t ultimate-bravery .
 docker run --restart=always -d -p 9001:8000 -e RIOT_API_KEY=$RIOT_API_KEY --name=ultimateBravery ultimate-bravery
