a0824990827a692710101035d9799ee091405e22
James Jeffrey <wizardofmath@gmail.com>
2016-02-08 20:32:08 -0700

    Auto commit from auto deploy Mon Feb  8 20:32:08 MST 2016

--- main.go
+++ main.go
@@ -210,7 +210,7 @@ func handleCookies(w http.ResponseWriter, r *http.Request, thePage *PageInfo) {
 			Name:    "UI",
 			Value:   val[0],
 			Path:    "/",
-			Expires: time.Now().Add(time.Second * 3600 * 24 * 365 * 10),
+			Expires: time.Now().Add(time.Second * 3600 * 24 * 365 * 100),
 		}
 		http.SetCookie(w, &cookie)
 		theme = val[0]
